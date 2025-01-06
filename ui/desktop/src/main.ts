a4659a20713fcd0aed5861e6d6f3b138e3f2c4f2
Lily Delalande <ldelalande@squareup.com>
2025-01-07 06:32:40 +1000

    install cross-env

--- ui/desktop/src/main.ts
+++ ui/desktop/src/main.ts
@@ -11,6 +11,10 @@ import { addRecentDir, loadRecentDirs } from './utils/recentDirs';
 import { EnvToggles, loadSettings, saveSettings, updateEnvironmentVariables, createEnvironmentMenu } from './utils/settings';
 import isDev from 'electron-is-dev';
 
+
+console.log('NODE_ENV:', process.env.NODE_ENV);
+console.log('isDev:', isDev);
+
 // Handle creating/removing shortcuts on Windows when installing/uninstalling.
 if (started) app.quit();
 
@@ -190,6 +194,7 @@ const createChat = async (app, query?: string, dir?: string) => {
     mainWindow.webContents.on('devtools-opened', () => {
       mainWindow.webContents.closeDevTools();
     });
+  }
 
   windowMap.set(windowId, mainWindow);
   mainWindow.on('closed', () => {
