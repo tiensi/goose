7ddc6a443f36e0dbcdd3f9a9100d33c85435266e
Nahiyan Khan <nahiyan@squareup.com>
2025-01-13 18:48:32 -0500

    style: swapped icons to arcade

---
+++ ui/desktop/src/components/icons/index.tsx
@@ -0,0 +1,11 @@
+import Attach from "./Attach";
+import Back from "./Back";
+import Check from "./Check";
+import ChevronDown from "./ChevronDown";
+import Close from "./Close";
+import Copy from "./Copy";
+import Hidden from "./Hidden";
+import More from "./More";
+import Send from "./Send";
+
+export { Attach, Back, Check, ChevronDown, Close, Copy, Hidden, More, Send };
